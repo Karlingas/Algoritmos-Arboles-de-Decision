80693bf258529b9504c91fe7959ceabd321e933c
Karlingas <dgdcuenca@gmail.com>
2025-02-19 08:49:37 +0100

    creado el archivo pruebas.py con los imports

---
+++ pruebas.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from sklearn import tree 
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+import seaborn as sns
+import random
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from collections import Counter
